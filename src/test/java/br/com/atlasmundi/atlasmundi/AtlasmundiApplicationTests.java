a28908e9ab38bd8070026404a6adcd8330ddf90c
pehgomes <pedro.gomes@beblue.com.br>
2022-03-18 00:32:00 -0300

    first commit

---
+++ src/test/java/br/com/atlasmundi/atlasmundi/AtlasmundiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atlasmundi.atlasmundi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasmundiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
